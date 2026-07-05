ba4492b79aebee2a8ba1ff712051f26193bc3b5e
Ryan Fitzgerald <rwfitzge@gmail.com>
2026-07-05 10:02:16 -0700

    Update STWL to July 1st, 2026 release

--- src/backtracking_search.rs
+++ src/backtracking_search.rs
@@ -1197,11 +1197,11 @@ mod tests {
             render_grid(&grid_config.to_config_ref(), &result_1.choices),
             indoc! {"
             .ass...
-            .dais..
-            barmenu
-            attires
-            merlins
-            ..eeee.
+            .glib..
+            alamosa
+            retiree
+            reelers
+            ..seat.
             ...sss.
             "}
             .trim()
@@ -1214,14 +1214,14 @@ mod tests {
             )
         };
 
-        let barmenu_id = get_id(&grid_config.word_list, "barmenu");
+        let retiree_id = get_id(&grid_config.word_list, "retiree");
         let sss_id = get_id(&grid_config.word_list, "sss");
 
         grid_config
             .word_list
             .dupe_index
             .as_mut()
-            .add_dupe_pair(barmenu_id, sss_id);
+            .add_dupe_pair(retiree_id, sss_id);
 
         let result_2 =
             find_fill(&grid_config.to_config_ref(), None, None).expect("Failed to find a fill");
@@ -1230,11 +1230,11 @@ mod tests {
             render_grid(&grid_config.to_config_ref(), &result_2.choices),
             indoc! {"
             .ass...
-            .nail..
-            hormone
-            attires
-            merlins
-            ..eeee.
+            .glia..
+            glamper
+            retinas
+            reelers
+            ..sean.
             ...sss.
             "}
             .trim()
